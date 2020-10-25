2646d9e089d3ea2caa275249651d3fe675f26d93
Saumiya <saumiya1994@gmail.com>
2020-10-26 01:39:40 +0530

    Update the GET, POST and DELETE with JPA repo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user values(1, sysdate(), 'AB');
-insert into user values(2, sysdate(), 'Jill');
-insert into user values(3, sysdate(), 'Jam');
+insert into user values(1111, sysdate(), 'AB');
+insert into user values(2111, sysdate(), 'Jill');
+insert into user values(3111, sysdate(), 'Jam');
