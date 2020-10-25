198be4535a3d74598f296b337d2dab907e0cbb5f
Saumiya <saumiya1994@gmail.com>
2020-10-26 02:23:35 +0530

    POST for Posts

--- src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rest.webservices.restfulwebservices.user.post.Post;
+import com.rest.webservices.restfulwebservices.user.post.PostRepository;
 
 @RestController
 public class UserJPAResource {
@@ -31,6 +32,9 @@ public class UserJPAResource {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private PostRepository postRepository;
 	
 	@GetMapping("/jpa/users")
 	public List<User> retrieveAllUsers() {
@@ -95,6 +99,28 @@ public class UserJPAResource {
 		}
 		
 		return userOptional.get().getPosts();
+	}
+	
+	@PostMapping("/jpa/users/{id}/posts")
+	public ResponseEntity<Object> createPost(@PathVariable int id, @RequestBody Post post) {
+		
+		Optional<User> userOptional = userRepository.findById(id);
+		
+		if(!userOptional.isPresent()) {
+			throw new UserNotFoundException("id-" + id);
+		}
+
+		User user = userOptional.get();
+		
+		post.setUser(user);
+		
+		postRepository.save(post);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
+
 	}
 
 }
