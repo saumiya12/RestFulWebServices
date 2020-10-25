89c64d0198d40f61571aa3c575ca1216946d72a3
Saumiya <saumiya1994@gmail.com>
2020-10-25 23:59:10 +0530

    UserEntity and H2 connection

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -3,6 +3,9 @@ package com.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -10,9 +13,11 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description=" Details of the user")
+@Entity
 public class User {
 
-
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast 2 characters" )
